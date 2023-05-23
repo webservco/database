98630a1c3d4a5559aab5a7865d83dd47f89841f0
Radu Maier <ariademur@webserv.co>
2023-05-23 15:19:16 +0200

    PDOConfigurationMySQLFactory

---
+++ src/WebServCo/Database/Factory/PDOConfigurationMySQLFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+
+namespace WebServCo\Database\Factory;
+
+use WebServCo\Database\Contract\PDOConfigurationFactoryInterface;
+use WebServCo\Database\DataTransfer\PDOConfiguration;
+
+final class PDOConfigurationMySQLFactory implements PDOConfigurationFactoryInterface
+{
+    public function __construct(
+        private string $host,
+        private int $port,
+        private string $dbname,
+        private string $username,
+        private string $password,
+    ) {
+    }
+
+    public function createPDOConfiguration(): PDOConfiguration
+    {
+        return new PDOConfiguration('mysql', $this->host, $this->port, $this->dbname, $this->username, $this->password);
+    }
+}
