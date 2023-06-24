a6620dcda12dcbcccb9e447fdb6b343c5ead6d7f
GitHub Action <action@github.com>
2023-06-24 20:04:46 +0000

    release PHP 7.4 downgraded v0.0.5

--- src/WebServCo/Database/Factory/PDOContainerMySQLFactory.php
+++ src/WebServCo/Database/Factory/PDOContainerMySQLFactory.php
@@ -8,15 +8,19 @@ use WebServCo\Database\Container\PDOContainer;
 
 final class PDOContainerMySQLFactory
 {
-    public function __construct(
-        private string $host,
-        private int $port,
-        private string $dbname,
-        private string $username,
-        private string $password,
-    ) {
+    private string $host;
+    private int $port;
+    private string $dbname;
+    private string $username;
+    private string $password;
+    public function __construct(string $host, int $port, string $dbname, string $username, string $password)
+    {
+        $this->host = $host;
+        $this->port = $port;
+        $this->dbname = $dbname;
+        $this->username = $username;
+        $this->password = $password;
     }
-
     public function createPDOContainer(): PDOContainer
     {
         return new PDOContainer(
